52c1f5778e84a63b6ecb7e13d030a84c8b2bd414
gaydov <gaidov@gmail.com>
2017-07-26 22:33:47 +0300

    upload

---
+++ 3. Iterators and Comparators/BookComparer/Models/BookComparator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class BookComparator : IComparer<Book>
+{
+    public int Compare(Book x, Book y)
+    {
+        int compareResult = x.Title.CompareTo(y.Title);
+
+        if (compareResult == 0)
+        {
+            compareResult = y.Year.CompareTo(x.Year);
+        }
+
+        return compareResult;
+    }
+}
---
+++ 3. Iterators and Comparators/ComparableBook/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book : IComparable<Book>
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.Authors = authors;
+    }
+
+    public string Title { get; }
+    public int Year { get; }
+    public IList<string> Authors { get; }
+
+    public int CompareTo(Book otherBook)
+    {
+        if (this.Year - otherBook.Year != 0)
+        {
+            return this.Year.CompareTo(otherBook.Year);
+        }
+
+        return this.Title.CompareTo(otherBook.Title);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+}
