07e453128408c09dee4c911bb9ade2f1cb426483
gaydov <gaidov@gmail.com>
2017-07-27 21:31:21 +0300

    upload

---
+++ 3. Iterators and Comparators/ListyIteratorPgm/Launcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIteratorPgm
+{
+    public class Launcher
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            ListyIterator<string> iterator = null;
+
+            while (!input.Equals("END"))
+            {
+                string[] args = input.Split();
+
+                switch (args[0])
+                {
+                    case "Create":
+
+                        iterator = new ListyIterator<string>(args.Skip(1));
+                        break;
+
+                    case "Move":
+                        Console.WriteLine(iterator.Move());
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(iterator.HasNext());
+                        break;
+
+                    case "Print":
+
+                        try
+                        {
+                            Console.WriteLine(iterator.Print());
+
+                        }
+                        catch (ArgumentException ae)
+                        {
+                            Console.WriteLine(ae.Message);
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 3. Iterators and Comparators/ListyIteratorPgm/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ListyIterator<T>
+{
+    private IList<T> elements;
+    private int currentIndex;
+
+    public ListyIterator(IEnumerable<T> inputElements)
+    {
+        this.elements = new List<T>(inputElements);
+    }
+
+    public bool Move()
+    {
+        if (HasNext())
+        {
+            this.currentIndex++;
+            return true;
+        }
+        return false;
+    }
+
+    public bool HasNext()
+    {
+        if (this.currentIndex < this.elements.Count - 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public T Print()
+    {
+        if (this.elements.Count == 0)
+        {
+            throw new ArgumentException("Invalid Operation!");
+        }
+
+        return this.elements[this.currentIndex];
+    }
+}
+
