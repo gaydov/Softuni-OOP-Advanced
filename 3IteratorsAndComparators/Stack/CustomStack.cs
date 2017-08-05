775ed66f6eb8750c02942544ee99a403df852be3
gaydov <gaidov@gmail.com>
2017-08-06 01:06:44 +0300

    implement the logic

--- 3IteratorsAndComparators/Stack/CustomStack.cs
+++ 3IteratorsAndComparators/Stack/CustomStack.cs
@@ -1,3 +1,63 @@
-﻿public class CustomStack
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CustomStack<T> : IEnumerable<T>
 {
+    private const int InitialCapacity = 8;
+    private T[] elements;
+    private int currentIndex;
+
+    public CustomStack()
+    {
+        this.elements = new T[InitialCapacity];
+        this.currentIndex = 0;
+    }
+
+    public void Push(IEnumerable<T> inputElements)
+    {
+        foreach (T element in inputElements)
+        {
+            // If our stack is full:
+            if (this.currentIndex == this.elements.Length)
+            {
+                this.Resize();
+            }
+
+            this.elements[this.currentIndex] = element;
+            this.currentIndex++;
+        }
+    }
+
+    public T Pop()
+    {
+        // If the stack is empty:
+        if (this.currentIndex == 0)
+        {
+            throw new InvalidOperationException("No elements");
+        }
+
+        T popedElement = this.elements[this.currentIndex - 1];
+        this.elements[this.currentIndex - 1] = default(T);
+        this.currentIndex--;
+        return popedElement;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = this.currentIndex - 1; i >= 0; i--)
+        {
+            yield return this.elements[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private void Resize()
+    {
+        Array.Resize(ref this.elements, this.elements.Length * 2);
+    }
 }
--- 3IteratorsAndComparators/Stack/Launcher.cs
+++ 3IteratorsAndComparators/Stack/Launcher.cs
@@ -1,9 +1,51 @@
-﻿namespace Stack
+﻿using System;
+using System.Linq;
+
+namespace Stack
 {
     public class Launcher
     {
         public static void Main()
         {
+            CustomStack<int> myStack = new CustomStack<int>();
+            string input = Console.ReadLine();
+
+            while (!input.Equals("END"))
+            {
+                string[] args = input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                string command = args[0];
+
+                switch (command)
+                {
+                    case "Push":
+
+                        myStack.Push(args.Skip(1).Select(int.Parse));
+                        break;
+
+                    case "Pop":
+
+                        try
+                        {
+                            myStack.Pop();
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (int element in myStack)
+                {
+                    Console.WriteLine(element);
+                }
+            }
         }
     }
 }
