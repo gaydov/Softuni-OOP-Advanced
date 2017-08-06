525355bdd5ca3fcbc6f7f08aa660b9f5594b7e65
gaydov <gaidov@gmail.com>
2017-08-06 20:23:48 +0300

    change the way of capitilizing the command's name

--- 5Reflection/BarrackWarsTasks/Core/CommandInterpreter.cs
+++ 5Reflection/BarrackWarsTasks/Core/CommandInterpreter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using BarrackWarsTasks.Contracts;
@@ -8,6 +9,9 @@ namespace BarrackWarsTasks.Core
 {
     public class CommandInterpreter : ICommandInterpreter
     {
+        private const string CommandClassSuffix = "Command";
+        private const string FolderWithCommands = "Commands";
+
         public IExecutable InterpretCommand(string[] data, string commandName)
         {
             // Getting the namespace where the commands reside
@@ -17,9 +21,9 @@ namespace BarrackWarsTasks.Core
                 .Select(t => t.Namespace)
                 .Distinct()
                 .Where(n => n != null)
-                .FirstOrDefault(n => n.Contains("Commands"));
+                .FirstOrDefault(n => n.Contains(FolderWithCommands));
 
-            string actualCommandName = char.ToUpper(commandName[0]) + commandName.Substring(1) + "Command";
+            string actualCommandName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(commandName) + CommandClassSuffix;
             Type classType = Type.GetType($"{commandsNamespace}.{actualCommandName}");
             IExecutable command = (Command)Activator.CreateInstance(classType, new object[] { data });
 
