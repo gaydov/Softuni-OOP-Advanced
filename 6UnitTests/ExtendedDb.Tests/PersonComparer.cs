e82ca5a104cb89ed03a445cfeebeba7084bd5556
gaydov <gaidov@gmail.com>
2017-08-08 23:34:10 +0300

    change GetHashCode() logic

--- 6UnitTests/ExtendedDb.Tests/PersonComparer.cs
+++ 6UnitTests/ExtendedDb.Tests/PersonComparer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using ExtendedDatabase.Models;
 
 namespace ExtendedDb.Tests
@@ -11,9 +10,9 @@ namespace ExtendedDb.Tests
             return x.Username.Equals(y.Username) && x.Id == y.Id;
         }
 
-        public int GetHashCode(Person obj)
+        public int GetHashCode(Person personObj)
         {
-            return Tuple.Create(obj.Username, obj.Id).GetHashCode();
+            return personObj.Username.GetHashCode() + personObj.Id.GetHashCode();
         }
     }
 }
